06f390f2102332b154800b32e00a8a4f5dca1929
shelpii <umbrasasdima@gmail.com>
2024-03-13 20:44:59 +0200

    added project architecture

---
+++ src/hooks/http.hooks.js
@@ -0,0 +1,18 @@
+export const useHttp = () => {
+    const request = async url => {
+        try {
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error(`Could not fetch: ${url} - status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            return data;
+        } catch (e) {
+            throw e;
+        }
+    };
+    return { request };
+};
